f29734c1195961a957f1b1561a3a5cb21e70ef66
Sam Murphy <stmurphy@agiledigital.com.au>
2020-12-09 10:58:44 +1100

    Add api gateway

--- diagnostic-tool/src/handlers/test3.ts
+++
@@ -1,7 +0,0 @@
-import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-export const handler = async (
-  event: APIGatewayEvent
-): Promise<APIGatewayProxyResult> => {
-  return { statusCode: 200, body: event.body ?? '' };
-};
