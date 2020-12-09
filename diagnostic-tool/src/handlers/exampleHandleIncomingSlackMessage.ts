d8f5337581cda26b24009dc11a16ccd1ecb910b7
Robin McAndrew <robin.mcandrew@agiledigital.com.au>
2020-12-09 17:02:15 +1100

    Fix json douple stringify

--- diagnostic-tool/src/handlers/exampleHandleIncomingSlackMessage.ts
+++ diagnostic-tool/src/handlers/exampleHandleIncomingSlackMessage.ts
@@ -1,44 +1,40 @@
-import {SQSEvent} from 'aws-lambda';
-import {SNS} from "aws-sdk";
-import {cleanEnv, str} from "envalid";
-import {SlackCommandSnsEvent} from "../types";
-
+import { SQSEvent } from 'aws-lambda';
+import { SNS } from 'aws-sdk';
+import { cleanEnv, str } from 'envalid';
+import { SlackCommandSnsEvent } from '../types';
 
 const sns = new SNS();
 
 const env = cleanEnv(process.env, {
   SLACK_SIGNING_SECRET: str(),
   SLACK_BOT_TOKEN: str(),
-  OUTGOING_SNS_TOPIC_ARN: str()
-})
-
+  OUTGOING_SNS_TOPIC_ARN: str(),
+});
 
 export const handler = async (event: SQSEvent) => {
   console.log(JSON.stringify(event, undefined, 2));
 
-  const bodies = event.Records.map(({body}) => JSON.parse(JSON.parse(body).Message) as SlackCommandSnsEvent )
+  const bodies = event.Records.map(
+    ({ body }) => JSON.parse(JSON.parse(body).Message) as SlackCommandSnsEvent
+  );
 
-  const testPayload = (index: string) => (
-      [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `SENT FROM OUTGOING SNS TOPIC HANDLER.\n message number ${index}`,
-          },
-        }
-      ]
-    )
-  ;
-
-
+  const testPayload = (index: string) => [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `SENT FROM OUTGOING SNS TOPIC HANDLER.\n message number ${index}`,
+      },
+    },
+  ];
   const sendMessage = async (body: SlackCommandSnsEvent) => {
+    const outgoingMessages = [testPayload('1'), testPayload('2')];
 
-    const outgoingMessages = [testPayload("1"), testPayload("2")];
-
-    const messages = await outgoingMessages.map(async message => {
-
-      const outgoingPayload = JSON.stringify({originalMessage: body, message: message});
+    const messages = await outgoingMessages.map(async (message) => {
+      const outgoingPayload = {
+        originalMessage: body,
+        message: message,
+      };
       console.log(outgoingPayload);
       await sns
         .publish({
@@ -46,14 +42,11 @@ export const handler = async (event: SQSEvent) => {
           Message: JSON.stringify(outgoingPayload),
         })
         .promise();
-    })
-    return Promise.all(messages)
+    });
+    return Promise.all(messages);
   };
 
-  await Promise.all(bodies.map(async body => await sendMessage(body)))
+  await Promise.all(bodies.map(async (body) => await sendMessage(body)))
     .catch((error) => console.error(error))
-    .finally(() => console.log("done"))
-
-
+    .finally(() => console.log('done'));
 };
-
--- diagnostic-tool/src/handlers/handleOutgoingSlackMessage.ts
+++ diagnostic-tool/src/handlers/handleOutgoingSlackMessage.ts
@@ -1,9 +1,9 @@
-import {SQSEvent} from 'aws-lambda';
+import { SQSEvent } from 'aws-lambda';
 
-import {App, ExpressReceiver} from '@slack/bolt';
-import {SlackCommandSnsEvent} from "../types";
-import {cleanEnv, str} from "envalid";
-import {Block, KnownBlock} from "@slack/types";
+import { App, ExpressReceiver } from '@slack/bolt';
+import { SlackCommandSnsEvent } from '../types';
+import { cleanEnv, str } from 'envalid';
+import { Block, KnownBlock } from '@slack/types';
 
 export const handler = async (event: SQSEvent) => {
   // eslint-disable-next-line no-console
@@ -12,14 +12,16 @@ export const handler = async (event: SQSEvent) => {
   const env = cleanEnv(process.env, {
     SLACK_SIGNING_SECRET: str(),
     SLACK_BOT_TOKEN: str(),
-  })
+  });
 
   interface IncomingSqsMessage {
-    originalMessage: SlackCommandSnsEvent,
-    message: (KnownBlock | Block)[]
+    originalMessage: SlackCommandSnsEvent;
+    message: (KnownBlock | Block)[];
   }
 
-  const bodies = event.Records.map(({body}) => JSON.parse(JSON.parse(body).Message)  as IncomingSqsMessage)
+  const bodies = event.Records.map(
+    ({ body }) => JSON.parse(JSON.parse(body).Message) as IncomingSqsMessage
+  );
 
   const expressReceiver = new ExpressReceiver({
     signingSecret: env.SLACK_SIGNING_SECRET || '',
@@ -33,20 +35,24 @@ export const handler = async (event: SQSEvent) => {
     processBeforeResponse: true,
   });
 
-  const sendMessage = (incomingMessage: IncomingSqsMessage) => app.client.chat.postMessage({
-    thread_ts: incomingMessage.originalMessage.messageThreadKey,
-    token: env.SLACK_BOT_TOKEN,
-    channel: incomingMessage.originalMessage.channel,
-    blocks: incomingMessage.message,
-    text: 'More logs for you to look at',
+  const sendMessage = (incomingMessage: IncomingSqsMessage) =>
+    app.client.chat.postMessage({
+      thread_ts: incomingMessage.originalMessage.messageThreadKey,
+      token: env.SLACK_BOT_TOKEN,
+      channel: incomingMessage.originalMessage.channel,
+      blocks: incomingMessage.message,
+      text: 'More logs for you to look at',
+    });
 
-  });
-
-  const messagesOutgoing = bodies.map(message => sendMessage(message))
+  const messagesOutgoing = bodies.map((message) => sendMessage(message));
 
   const results = await Promise.all(messagesOutgoing)
     .catch((errors) => console.error(errors))
-    .finally(() => console.log("success in sending all messages: " + new Date().toDateString()));
+    .finally(() =>
+      console.log(
+        'success in sending all messages: ' + new Date().toDateString()
+      )
+    );
 
   // eslint-disable-next-line no-console
   console.log(results);
