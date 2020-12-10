40948f827ec40929eac64826c88dc4d836c6d8d9
Sam Murphy <stmurphy@agiledigital.com.au>
2020-12-10 11:40:29 +1100

    feat: Add cloudtrail inspector

--- diagnostic-tool/src/handlers/inspectors/collectCloudTrail.ts
+++ diagnostic-tool/src/handlers/inspectors/collectCloudTrail.ts
@@ -1,9 +1,42 @@
-import { SNSMessage, SQSEvent, SQSRecord } from 'aws-lambda';
-import { CloudTrail } from 'aws-sdk';
+import { SectionBlock } from '@slack/bolt';
+import { SQSEvent, SQSRecord } from 'aws-lambda';
+import { CloudTrail, SNS } from 'aws-sdk';
 import { EventsList } from 'aws-sdk/clients/cloudtrail';
-import { extractSlackCommand } from '../common';
+import { SleuthBotIncomingRequest } from '../../types';
+import { extractSlackCommand, sendOutgoingMessage } from '../common';
 
 const cloudTrail = new CloudTrail();
+const sns = new SNS();
+
+type Fetcher = (
+  serviceScope: string | undefined
+) => (payload: string | undefined) => string | undefined;
+
+const lambdaDataFetcher: Fetcher = (serviceScope) => (payload) => {
+  if (payload === undefined) {
+    return undefined;
+  }
+
+  const data = <
+    { responseElements: { functionName: string }; eventName: string }
+  >JSON.parse(payload);
+
+  if (data.responseElements.functionName.includes(serviceScope ?? '')) {
+    return `\`${data.responseElements.functionName}\``;
+  }
+
+  return undefined;
+};
+
+const serviceMapping: Record<string, Fetcher> = {
+  'lambda.amazonaws.com': lambdaDataFetcher,
+  // eslint-disable-next-line unused-imports/no-unused-vars-ts
+  unknown: (_) => (_) => undefined,
+};
+const serviceFilter = Object.keys(serviceMapping);
+
+const eventNameStripping = (name: string) =>
+  name.substring(0, /[0-9]/.exec(name)?.index);
 
 const delay = async (timeout: number) =>
   new Promise<void>((resolve) => {
@@ -31,50 +64,69 @@ const fetchResults = async (
   return result.Events ?? [];
 };
 
-export const handler = async (event: SQSEvent) => {
-  return Promise.allSettled(
-    event.Records.map(async (sqsRecord: SQSRecord) => {
-      const {} = extractSlackCommand(sqsRecord);
+export const sendMessage = async (
+  lines: string[],
+  originalMessage: SleuthBotIncomingRequest
+): Promise<void> => {
+  const logLines: SectionBlock[] = lines.map((line) => ({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: line,
+    },
+  }));
 
-      const results = await fetchResults(new Date());
-
-      return results.map(
-        ({
-          EventId,
-          EventName,
-          EventTime,
-          Username,
-          EventSource,
-          CloudTrailEvent,
-        }) => ({
-          EventId,
-          EventName,
-          EventTime,
-          EventSource,
-          Username,
-          CloudTrailEvent,
-        })
-      );
-    })
+  await sendOutgoingMessage(
+    {
+      originalMessage,
+      message: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text:
+              "🔎 Audit Inspector here! I've fetched you some CloudTrail events that might be relevant. Hope it helps!",
+          },
+        },
+        ...logLines,
+      ],
+    },
+    sns
   );
 };
 
-(async () => {
-  const result = await handler({
-    Records: [
-      // @ts-ignore
-      {
-        body: JSON.stringify({
-          Message: JSON.stringify({ text: 'Test' }),
-        } as SNSMessage),
-      },
-    ],
-  });
+export const handler = async (event: SQSEvent) => {
+  const formattedMessages = await Promise.allSettled(
+    event.Records.map(async (sqsRecord: SQSRecord) => {
+      const request = extractSlackCommand(sqsRecord);
 
-  // eslint-disable-next-line no-console
-  console.dir(
-    result.map((value) => value),
-    { depth: undefined, colors: true }
+      const results = await fetchResults(new Date());
+
+      const filterAwsServices = results.filter(({ EventSource }) =>
+        serviceFilter.includes(EventSource ?? '')
+      );
+
+      const formattedPayloads = filterAwsServices
+        .map(({ EventName, Username, CloudTrailEvent, EventSource }) => ({
+          EventName: eventNameStripping(EventName ?? ''),
+          Username,
+          EventSource: EventSource?.split('.')[0],
+          message: serviceMapping['unknown'](request.text)(CloudTrailEvent),
+        }))
+        .map(
+          ({ EventName, Username, EventSource, message }) =>
+            `*Event*: ${EventName}\n*User*: ${Username}\n*Source*: ${EventSource}\n${message}`
+        );
+
+      return { messages: formattedPayloads, request };
+    })
   );
-  console.info(result.length);
-})();
+
+  await Promise.allSettled(
+    formattedMessages.map(async (value) =>
+      value.status === 'fulfilled'
+        ? sendMessage(value.value.messages, value.value.request)
+        : undefined
+    )
+  );
+};
